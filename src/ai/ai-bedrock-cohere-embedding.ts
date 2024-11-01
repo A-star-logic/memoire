b4df8d646ad84aaa8fba2b36d17f7dd2e0578cd1
Sriram Vinnakota <21726955-sriramvinnakota@users.noreply.gitlab.com>
2024-11-01 16:01:46 +0000

    updated the logic and removed pre-processing

--- src/ai/ai-bedrock-cohere-embedding.ts
+++ src/ai/ai-bedrock-cohere-embedding.ts
@@ -1,15 +1,40 @@
-/* eslint-disable no-console */
 import {
   BedrockRuntimeClient,
   InvokeModelCommand,
 } from '@aws-sdk/client-bedrock-runtime';
-import { Tiktoken } from 'tiktoken/lite';
 import { TokenTextSplitter } from '@langchain/textsplitters';
 import cl100k from 'tiktoken/encoders/cl100k_base.json';
-import type {
-  CohereEmbeddingBody,
-  CohereEmbeddingResponse,
-} from './bedrock-embedding-service-schemas.js';
+import { Tiktoken } from 'tiktoken/lite';
+/**
+ * https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-embed.html
+ */
+interface CohereEmbeddingBody {
+  // Specifies the types of embeddings you want to have returned, can be multiple format
+  embedding_types?: string[];
+  // Differentiate each type from one another. You should not mix different types together
+  input_type:
+    | 'classification' // for text classifiation tasks
+    | 'clustering' // for text clustering tasks
+    | 'search_document' // To embed serach doc in database
+    | 'search_query'; // To embed query
+  // Array of strings to embed
+  texts: string[];
+  // specifies the truncate possion if input exceeds token limit, suggested to leave NONE(default) since we handle the tokens
+  truncate?: 'END' | 'NONE' | 'START';
+}
+/**
+ * https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-embed.html
+ */
+interface CohereEmbeddingResponse {
+  // response ID
+  id: string;
+  // input texts for the model
+  texts: string[];
+  // array of embeddings, each embedding with 1024 length
+  embeddings: number[][];
+  // type of the response(embeddings_floats or embeddings_by_type)
+  response_type: string;
+}
 
 if (!process.env.AWS_REGION) {
   throw new Error('Please set AWS_REGION');
@@ -48,51 +73,53 @@ function exceedsTokenLimit({ chunk }: { chunk: string }): boolean {
 }
 
 /**
+ * Asynchronously wait for a number of milliseconds.
+ *
+ * When using this function to rate limit API calls, **YOU MUST ENSURE** that only one call is being made when using this method, or it will be useless
+ * @param ms The number of milliseconds to wait
+ * @returns void
+ */
+function sleep(ms: number): Promise<unknown> {
+  return new Promise((resolve) => {
+    return setTimeout(resolve, ms);
+  });
+}
+
+/**
  * Call the bedrock embedding model
  * @param root named parameters
- * @param root.chunks A document, or a list of chunks from a document to embed
- * @param root.isQuery is the documnt for query
+ * @param root.chunk A document whitin the model input token limit
+ * @param root.embeddingTask task the embedding needed for
  * @returns an object with the averaged embedding an an array of chunk embeddings
  */
 export async function invokeBedrockCohereEmbedding({
-  chunks,
-  isQuery,
+  chunk,
+  embeddingTask,
 }: {
-  chunks: string[];
-  isQuery: boolean;
-}): Promise<CohereEmbeddingResponse[] | undefined> {
-  const processedChunks = await Promise.all(
-    chunks.map(async (chunk) => {
-      return chunk.replace('\n', '');
-    }),
-  );
+  chunk: string;
+  embeddingTask: CohereEmbeddingBody['input_type'];
+}): Promise<CohereEmbeddingResponse | undefined> {
+  await sleep(30); // cohere api call limit is 2000 per min
   try {
-    if (
-      chunks.some((chunk) => {
-        return exceedsTokenLimit({ chunk });
-      })
-    ) {
+    if (exceedsTokenLimit({ chunk })) {
       throw new Error('A document exceeds token limit');
     }
-    const embeddingResponses = Promise.all(
-      processedChunks.map(async (chunk) => {
-        const command = new InvokeModelCommand({
-          modelId: 'cohere.embed-english-v3',
-          body: JSON.stringify({
-            texts: [chunk],
-            input_type: isQuery ? 'search_query' : 'search_document',
-          } satisfies CohereEmbeddingBody),
-        });
-        const response = await bedrockClient.send(command);
-        const responseBody: CohereEmbeddingResponse = JSON.parse(
-          Buffer.from(response.body).toString('utf8'),
-        );
-        return responseBody;
-      }),
+
+    const command = new InvokeModelCommand({
+      modelId: 'cohere.embed-english-v3',
+      body: JSON.stringify({
+        texts: [chunk],
+        input_type: embeddingTask,
+      } satisfies CohereEmbeddingBody),
+    });
+    const response = await bedrockClient.send(command);
+    const responseBody: CohereEmbeddingResponse = JSON.parse(
+      Buffer.from(response.body).toString('utf8'),
     );
-    return embeddingResponses;
+    return responseBody;
   } catch (error) {
     const message = 'The embedding function had an error';
+    // eslint-disable-next-line no-console
     console.error({
       error,
       message,
@@ -119,36 +146,41 @@ export async function createDocumentChunks({
   });
 
   const chunks = await textSplitter.splitText(document);
-  return chunks.filter((chunk) => {
-    return chunk !== '\n';
-  });
+  return chunks;
 }
 
 /**
  * Embed a document or a list of chunks by cohere embedding model, if the document has been pre-splitted
  * @param root named parameters
- * @param root.document A document, or a list of chunks from a document to embed
- * @param root.isQuery is the documnt for query
- * @returns an object with an array of chunk embeddings and their ids
+ * @param root.document A document as a string
+ * @param root.embeddingTask task the embedding needed for
+ * @returns an object with the averaged embedding an an array of chunk embeddings
  */
 export async function embedDocumentWithCohere({
   document,
-  isQuery,
+  embeddingTask,
 }: {
-  document: string[];
-  isQuery: boolean;
+  document: string;
+  embeddingTask: CohereEmbeddingBody['input_type'];
 }): Promise<{ chunkID: string; embedding: number[] }[] | undefined> {
-  const chunks =
-    document.length > 1
-      ? document
-      : await createDocumentChunks({ document: document[0] });
-  const modelResponse = await invokeBedrockCohereEmbedding({ chunks, isQuery });
-  if (!modelResponse) {
-    return undefined;
+  const chunks = await createDocumentChunks({ document });
+
+  const modelResponses: CohereEmbeddingResponse[] = [];
+
+  for (const chunk of chunks) {
+    const modelResponse = await invokeBedrockCohereEmbedding({
+      chunk,
+      embeddingTask,
+    });
+    if (modelResponse === undefined) {
+      return undefined;
+    }
+
+    modelResponses.push(modelResponse);
   }
 
   return Promise.all(
-    modelResponse.map(async (embedding) => {
+    modelResponses.map(async (embedding) => {
       return { chunkID: embedding.id, embedding: embedding.embeddings[0] };
     }),
   );
