b630908b96717f36def662d18d033ddb5cb2eb95
Rohith Kulothungan <173175567+rk-ansearch@users.noreply.github.com>
2024-11-01 04:48:02 +0000

    adding extract content code

--- src/core/extractor.ts
+++ src/core/extractor.ts
@@ -1,7 +1,10 @@
+import axios from 'axios';
+import mammoth from 'mammoth';
+
 /**
  * Download a document from the given url
  * @param root named parameters
- * @param root.url the url of the document to download
+ * @param root.url the presigned url of the document to download
  * @returns the content of the document
  */
 export async function extractFromUrl({
@@ -9,5 +12,20 @@ export async function extractFromUrl({
 }: {
   url: string;
 }): Promise<string> {
-  return 'not implemented' + url;
+  const extension = url.split('.').pop();
+  const fileType = extension?.split('?')[0];
+  if (fileType === 'docx' || fileType === 'txt') {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const documentBuffer = Buffer.from(response.data);
+    if (fileType === 'docx') {
+      const result = await mammoth.extractRawText({
+        buffer: documentBuffer,
+      });
+      return result.value;
+    } else {
+      return documentBuffer.toString('utf8');
+    }
+  } else {
+    return 'file type not supported';
+  }
 }
