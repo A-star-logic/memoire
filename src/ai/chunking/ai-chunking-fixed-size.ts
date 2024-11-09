9c0f49a16048de08c054b8e8c48ecf779af5f3d8
Sriram Vinnakota <21726955-sriramvinnakota@users.noreply.gitlab.com>
2024-11-09 07:48:00 +0000

    updated agentic chunking and test cases

--- src/ai/chunking/ai-chunking-fixed-size.ts
+++ src/ai/chunking/ai-chunking-fixed-size.ts
@@ -5,17 +5,20 @@ import { TokenTextSplitter } from '@langchain/textsplitters';
  * @param root named parameters
  * @param root.document the document to split into chunks
  * @param root.chunkSize desired token size of the chunk
+ * @param root.chunkOverlap number of tokens to overlap, more to LLM to recognice previos chunk
  * @returns a list of chunks
  */
 export async function createDocumentChunks({
+  chunkOverlap = 0,
   chunkSize = 512,
   document,
 }: {
+  chunkOverlap?: number;
   chunkSize?: number;
   document: string;
 }): Promise<string[]> {
   const textSplitter = new TokenTextSplitter({
-    chunkOverlap: 0,
+    chunkOverlap,
     chunkSize,
     encodingName: 'cl100k_base',
   });
