07cb5c2c3ea6815405006961ff09e760c53a2f1a
Rohith Kulothungan <173175567+rk-ansearch@users.noreply.github.com>
2025-03-17 15:58:39 +0000

    Removing AI models except cohere

--- services/apps/memoire/src/ai/embedding/model/index.ts
+++ services/apps/memoire/src/ai/embedding/model/index.ts
@@ -6,69 +6,11 @@
 
 import { logger } from '@astarlogic/services-database/reporting';
 
-let embedDocumentFunction;
-let embedQueryFunction;
-let isTooLargeFunction;
-
-switch (process.env.EMBEDDING_MODEL) {
-  case 'cohere': {
-    const cohereModel = await import('./ai-embedding-model-cohere.js');
-    embedDocumentFunction = cohereModel.embedDocument;
-    embedQueryFunction = cohereModel.embedQuery;
-    isTooLargeFunction = cohereModel.isTooLarge;
-    logger.info('Using Cohere model for embedding');
-    break;
-  }
-
-  case 'openai': {
-    const openaiModel = await import('./ai-embedding-model-openai.js');
-    embedDocumentFunction = openaiModel.embedDocument;
-    embedQueryFunction = openaiModel.embedDocument;
-    isTooLargeFunction = openaiModel.isTooLarge;
-    logger.info('Using OpenAI model for embedding');
-    break;
-  }
-
-  case 'snowflake': {
-    const snowflakeLocalModel = await import(
-      './ai-embedding-model-local-snowflake.js'
-    );
-    embedDocumentFunction = snowflakeLocalModel.embedDocument;
-    embedQueryFunction = snowflakeLocalModel.embedDocument;
-    isTooLargeFunction = snowflakeLocalModel.isTooLarge;
-    logger.info('using local snowflake model for embedding');
-    break;
-  }
-
-  case 'titanG1': {
-    const titanModel = await import('./ai-embedding-model-titan-g1.js');
-    embedDocumentFunction = titanModel.embedDocument;
-    embedQueryFunction = titanModel.embedDocument;
-    isTooLargeFunction = titanModel.isTooLarge;
-    logger.info('Using Titan model for embedding');
-    break;
-  }
-
-  case 'titanV2': {
-    const titanModel = await import('./ai-embedding-model-titan-v2.js');
-    embedDocumentFunction = titanModel.embedDocument;
-    embedQueryFunction = titanModel.embedDocument;
-    isTooLargeFunction = titanModel.isTooLarge;
-    logger.info('Using Titan model for embedding');
-    break;
-  }
-
-  default: {
-    const xenovaLocalModel = await import(
-      './ai-embedding-model-local-xenova.js'
-    );
-    embedDocumentFunction = xenovaLocalModel.embedDocument;
-    embedQueryFunction = xenovaLocalModel.embedDocument;
-    isTooLargeFunction = xenovaLocalModel.isTooLarge;
-    logger.info('Using local model for embedding');
-    break;
-  }
-}
+const cohereModel = await import('./ai-embedding-model-cohere.js');
+const embedDocumentFunction = cohereModel.embedDocument;
+const embedQueryFunction = cohereModel.embedQuery;
+const isTooLargeFunction = cohereModel.isTooLarge;
+logger.info('Using Cohere model for embedding');
 
 export const embedDocument = embedDocumentFunction;
 export const embedQuery = embedQueryFunction;
